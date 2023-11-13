11d50564226b53807500c102280dc8405a67bdd8
Alex CROZAT <alex.crozat643@gmail.com>
2023-11-13 15:23:08 +0100

    Added on_mouse_press

--- game.py
+++ game.py
@@ -12,9 +12,14 @@ import config
 class Game:
 
     """ """
+
     def __init__(self):
         self.endgame = False
-        self.player = ship([config.MAP_SIZE[0] / 2, config.MAP_SIZE[1] / 2], config.SPEED_FACTOR, config.SIZE_SHIP)
+        self.player = ship(
+            [config.MAP_SIZE[0] / 2, config.MAP_SIZE[1] / 2],
+            config.SPEED_FACTOR,
+            config.SIZE_SHIP,
+        )
         self.asteroids = []
         self.ennemies = []
         self.time = 0
@@ -23,5 +28,4 @@ class Game:
         pass
 
     def update(self):
-        self.time += 
-
+        self.time += config.TICK_TIME
