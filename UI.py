b848328a7d4c6f35c2654b4442f7e48be0eb2730
Bronislav Abadie <Bronislawabadie@gmail.com>
2023-11-27 15:09:38 +0100

    suppression de sprite dans UI
    suppression de sprite dans UI

--- UI.py
+++ UI.py
@@ -19,36 +19,6 @@ def center_image(image):
     image.anchor_y = image.height // 2
 
 
-class Sprites:
-    def __init__(self, chemin_image=None, polygone=None):
-        """
-        Classe d'un sprite
-        => Implémente les collisions entre sprites
-        => Implémente l'affichage sur l'écran
-        => Peut être une image ou un polygone
-        args : - Nom de fichier
-               - Polygone sous forme de liste de points (x, y)
-        """
-        if chemin_image is not None:
-            self.sprite = pyglet.sprite.Sprite(pyglet.image.load(chemin_image))
-            self.type = "image"
-        elif polygone is not None:
-            self.sprite = pyglet.sprite.Sprite(polygone)
-            self.type = "polygon"
-        else:
-            raise ValueError("Il faut un chemin d'image ou un polygone")
-
-    def intersects(self, other):
-        match (self.type, other.type):
-            case ("image", "image"):
-                return self.sprite.intersects(other.sprite)
-            case ("image", "polygon"):
-                return self.sprite.intersects(other.sprite)
-            case ("polygon", "image"):
-                return self.sprite.intersects(other.sprite)
-            case ("polygon", "polygon"):
-                return self.sprite.intersects(other.sprite)
-        """Fonction qui calcule si deux sprites se touchent"""
 
 
 class GameUI:
