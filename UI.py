a7bc5f42b3b9604cf9e7a31cbcd2c52eafc459eb
Bilal59170 <bilal.ben-moussa@eleves.enpc.fr>
2024-01-10 15:19:18 +0100

    Feat: bouton

--- UI.py
+++ UI.py
@@ -9,18 +9,14 @@ class Button:
         self.text = pyglet.text.Label(text,
                                       font_name='Arial',
                                       font_size=18,
-                                      x=self.x, y=self.y,
+                                      color=(0, 0, 0, 255),
+                                      x=x, y=y,
                                       anchor_x='center', anchor_y='center', batch=batch)
-
-    def draw(self):
-        # Dessine le bouton
-        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
-                             ('v2i', [self.x - self.width // 2, self.y - self.height // 2,
-                                      self.x + self.width // 2, self.y - self.height // 2,
-                                      self.x + self.width // 2, self.y + self.height // 2,
-                                      self.x - self.width // 2, self.y + self.height // 2]),
-                             ('c3B', (255, 255, 255) * 4))
         
+        self.rectangle = pyglet.shapes.Rectangle(x - width/2, y - height/2, width, height, color=(255, 255, 255), batch = batch)
+
+
+
 
     def on_mouse_press(self, x, y, button, modifiers):
         if self.x - self.width // 2 < x < self.x + self.width // 2 and \
@@ -46,13 +42,11 @@ class StartMenu:
                                              x=self.window.width // 2, y=self.window.height // 2,
                                              anchor_x='center', anchor_y='center', batch=self.batch))
 
-        # self.play_button = Button(self.window.width // 4, self.window.height // 2 - 100, 100, 50, "Jouer", self.batch)
-        # self.quit_button = Button(3*(self.window.width // 4), self.window.height // 2 - 100, 100, 50, "Quitter", self.batch)
+        self.play_button = Button(self.window.width // 4, self.window.height // 2 - 100, 100, 50, "Jouer", self.batch)
+        self.quit_button = Button(3*(self.window.width // 4), self.window.height // 2 - 100, 100, 50, "Quitter", self.batch)
 
     def on_draw(self):
         self.window.clear()
-        self.play_button.draw()
-        self.quit_button.draw()
         self.batch.draw()
 
 
