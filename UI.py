03f3d35ffae304af5917bac0ff9102092ab2b31c
MathisWauquiezz <146733338+MathisWauquiezz@users.noreply.github.com>
2024-01-10 14:49:20 +0100

    feat: button

--- UI.py
+++ UI.py
@@ -1,6 +1,32 @@
 import pyglet
 
-class Menu:
+class Button:
+    def __init__(self, x, y, width, height, text, batch):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = pyglet.text.Label(text,
+                                      font_name='Arial',
+                                      font_size=18,
+                                      x=self.x, y=self.y,
+                                      anchor_x='center', anchor_y='center', batch=batch)
+
+    def draw(self):
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
+                             ('v2f', (self.x - self.width // 2, self.y - self.height // 2,
+                                      self.x + self.width // 2, self.y - self.height // 2,
+                                      self.x + self.width // 2, self.y + self.height // 2,
+                                      self.x - self.width // 2, self.y + self.height // 2)))
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if self.x - self.width // 2 < x < self.x + self.width // 2 and \
+            self.y - self.height // 2 < y < self.y + self.height // 2:
+            return True
+        return False
+
+# Exactement la même classe mais qui utilise la classe button
+class StartMenu:
     def __init__(self):
         self.window = pyglet.window.Window(width=800, height=600, caption="Game Menu")
         self.batch = pyglet.graphics.Batch()
@@ -17,28 +43,19 @@ class Menu:
                                              x=self.window.width // 2, y=self.window.height // 2,
                                              anchor_x='center', anchor_y='center', batch=self.batch))
 
-        self.play_button = pyglet.text.Label('Jouer',
-                                             font_name='Arial',
-                                             font_size=18,
-                                             x=self.window.width // 4, y=self.window.height // 2 - 100,
-                                             anchor_x='center', anchor_y='center', batch=self.batch)
-        
-        self.quit_button = pyglet.text.Label('Quitter',
-                                             font_name='Arial',
-                                             font_size=18,
-                                             x=3*(self.window.width // 4), y=self.window.height // 2 - 100,
-                                             anchor_x='center', anchor_y='center', batch=self.batch)
+        self.play_button = Button(self.window.width // 4, self.window.height // 2 - 100, 100, 50, "Jouer", self.batch)
+        self.quit_button = Button(3*(self.window.width // 4), self.window.height // 2 - 100, 100, 50, "Quitter", self.batch)
 
     def on_draw(self):
         self.window.clear()
         self.batch.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        if self.play_button.x - self.play_button.content_width // 2 < x < self.play_button.x + self.play_button.content_width // 2 and \
-            self.play_button.y - self.play_button.content_height // 2 < y < self.play_button.y + self.play_button.content_height // 2:
+        if self.play_button.on_mouse_press(x, y, button, modifiers):
             self.window.close()
             self.start_game()
 
-        elif self.quit_button.x - self.quit_button.content_width // 2 < x < self.quit_button.x + self.quit_button.content_width // 2 and \
-            self.quit_button.y - self.quit_button.content_height // 2 < y < self.quit_button.y + self.quit_button.content_height // 2:
+        elif self.quit_button.on_mouse_press(x, y, button, modifiers):
             self.window.close()
+
+class 
--- main.py
+++ main.py
@@ -53,7 +53,7 @@ from pyglet.window import key
 import game_logic
 from game import Game
 
-from UI import Menu
+from UI import StartMenu
 
 def start_game():
     game = Game(profile=False)
@@ -63,7 +63,7 @@ def start_game():
     game.run()
 
 if __name__ == "__main__":
-    menu = Menu()
+    menu = MenuButton()
     menu.start_game = start_game
     pyglet.app.run()
 
